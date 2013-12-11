8ca17b8e1ce8645be9b1e0da0983c31ff72f160a
NemanjaBoric <burgosoft@gmail.com>
2013-12-11 21:44:21 +0100

    * Initial commit

---
+++ src/info/nemanjaboric/akordi/Akordi.java
@@ -0,0 +1,15 @@
+package info.nemanjaboric.akordi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Akordi extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
